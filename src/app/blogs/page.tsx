e78efbe3097bf558800783d1209a5658ef51cf9f
aungthuoo <koaungthuoo@gmail.com>
2024-10-22 08:46:12 +0630

    fixed

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -5,6 +5,8 @@ import Navbar from "../components/navbar"
 export default async function Blogs() {
     return (
         <Container>
+
+            
             <Navbar/>
             <List />
         </Container>
